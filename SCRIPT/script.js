32cd05249309e6710a1d8c3aea1c9919f82bfca8
Naomi-Ashley <naomi.felter@hva.nl>
2022-12-13 23:43:54 +0100

    innit 1.3

---
+++ SCRIPT/script.js
@@ -0,0 +1,23 @@
+/*console.log ("OKE");
+
+var hetAntwoord = 42;
+
+hetAntwoord = "niet juist";
+
+var deVraag = hetAntwoord;
+
+console.log(deVraag );
+
+*/
+
+var resetButton = document.querySelector ('reset');
+
+function reset(){
+   if 
+   else 
+
+}
+
+resetButton.addEventListener ('click', reset);
+
+console.log('resetButton');
